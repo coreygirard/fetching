a34ac5c102a9d34515fa074e6f37132a943dbddf
Corey Girard <corey.r.girard@gmail.com>
2018-09-01 13:52:33 -0400

    Linting and testing

--- fetching.py
+++ fetching.py
@@ -1,40 +1,53 @@
-import copy
 import threading
 
+
 class Fetcher(object):
-    def __init__(self,c):
-        assert(hasattr(c,'fetch'))
+    def __init__(self, c):
+        assert hasattr(c, 'fetch')
         self.c = c
 
         # TODO: temporarily unmount the 'fetch' method to this class
 
-    def syncFetch(self):
+    def sync_fetch(self):
+        """Executes fetch function, then makes itself basically become
+        the fetched object"""
+
         self.c.fetch()
         self.__class__ = self.c.__class__
         self.__dict__ = self.c.__dict__
 
-    def asyncFetch(self):
-        self.thread = threading.Thread(target=self.syncFetch)
+    def async_fetch(self):
+        """Same as sync_fetch, but asynchronous"""
+
+        self.thread = threading.Thread(target=self.sync_fetch)
         self.thread.daemon = False
         self.thread.start()
 
-    def _callbackFetchHelper(self,f):
-        self.syncFetch()
+    def _callback_fetch_helper(self, f):
+        """Synchronous fetch and callback. Only used by callback_fetch"""
+
+        self.sync_fetch()
         f(self)
 
-    def callbackFetch(self,f):
-        self.thread = threading.Thread(target=self._callbackFetchHelper,
+    def callback_fetch(self, f):
+        """Perform async fetch and then trigger a callback function"""
+
+        self.thread = threading.Thread(target=self._callback_fetch_helper,
                                        args=(f,))
         self.thread.daemon = False
         self.thread.start()
 
 
 class FetchWhenNeeded(Fetcher):
-    def __getattr__(self,name):
-        self.syncFetch()
-        return getattr(self,name)
+    """Executes sync fetch as soon as attr access is attempted"""
+
+    def __getattr__(self, name):
+        self.sync_fetch()
+        return getattr(self, name)
 
 class FetchInBackground(Fetcher):
-    def __init__(self,c):
+    """Upon initialization, immediately begins async fetch"""
+
+    def __init__(self, c):
         super(FetchInBackground, self).__init__(c)
-        self.asyncFetch()
+        self.async_fetch()
--- test_fetching.py
+++ test_fetching.py
@@ -1,31 +1,116 @@
 import time
 from fetching import Fetcher, FetchWhenNeeded, FetchInBackground
+import pytest
 
-class Example(object):
-    def __init__(self,i):
-        self.i = i
+
+class Fetchable(object):
+    """Mock object for something like a webpage scraper"""
+
+    def __init__(self):
+        self.data = []
 
     def fetch(self):
-        self.parsed = []
-        for e in range(self.i):
-            self.parsed.append(e)
-            time.sleep(0.5)
+        self.data = [1, 2, 3]
 
-    def getParsed(self):
-        return self.parsed
+    def whatever(self):
+        return 4
 
-example = Example(10)
-example = FetchInBackground(example)
 
-print(example)
-time.sleep(6)
-print(example)
+class FetchableDelay(object):
+    """Mock object for something like a webpage scraper,
+    with delay to make async behavior obvious"""
 
-'''
-example = FetchWhenNeeded(example)
+    def __init__(self):
+        self.data = []
 
-print(example)
-print(example.getParsed())
-print(example.getParsed())
-print(example)
-'''
+    def fetch(self):
+        time.sleep(1)
+        self.data = [1, 2, 3]
+
+
+def test_Fetcher_init():
+    """Ensures that Fetcher init succeeds when the passed object
+    has a 'fetch' method, and fails when it does not"""
+
+    class NoFetch(object):
+        pass
+
+    Fetcher(Fetchable())
+
+    with pytest.raises(AssertionError):
+        Fetcher(NoFetch())
+
+
+def test_sync_fetch():
+    fetchable = Fetcher(Fetchable())
+    assert isinstance(fetchable, Fetcher)
+    fetchable.sync_fetch()
+    assert isinstance(fetchable, Fetchable)
+
+
+def test_async_fetch():
+    fetchable = Fetcher(FetchableDelay())
+    assert isinstance(fetchable, Fetcher)
+    start_time = time.time()
+    fetchable.async_fetch()
+
+    # ensure it is truly async
+    assert time.time() < start_time + 1
+    assert isinstance(fetchable, Fetcher)
+
+    time.sleep(1.5)
+    assert isinstance(fetchable, FetchableDelay)
+
+
+def test_callback_fetch():
+    class Logger(object):
+        def __init__(self):
+            self.logged = []
+
+        def __call__(self, obj):
+            self.logged.append(obj.data)
+
+    logger = Logger()
+
+    fetchable = Fetcher(FetchableDelay())
+
+    start_time = time.time()
+    fetchable.callback_fetch(logger)
+
+    # ensure it is truly async
+    assert time.time() < start_time + 1
+    assert isinstance(fetchable, Fetcher)
+
+    time.sleep(1.5)
+    assert isinstance(fetchable, FetchableDelay)
+
+    # ensure the callback function was actually called
+    assert logger.logged == [[1, 2, 3]]
+
+
+def test_FetchWhenNeeded():
+    fetchable = FetchWhenNeeded(Fetchable())
+    assert isinstance(fetchable, FetchWhenNeeded)
+    assert fetchable.data == [1, 2, 3]
+    assert isinstance(fetchable, Fetchable)
+
+    fetchable = FetchWhenNeeded(Fetchable())
+    assert isinstance(fetchable, FetchWhenNeeded)
+    assert fetchable.whatever() == 4
+    assert isinstance(fetchable, Fetchable)
+
+    with pytest.raises(AttributeError):
+        fetchable = FetchWhenNeeded(Fetchable())
+        fetchable.invalid()
+
+
+def test_FetchInBackground():
+    start_time = time.time()
+    fetchable = FetchInBackground(FetchableDelay())
+
+    # ensure it is truly async
+    assert time.time() < start_time + 1
+    assert isinstance(fetchable, FetchInBackground)
+
+    time.sleep(1.5)
+    assert isinstance(fetchable, FetchableDelay)
